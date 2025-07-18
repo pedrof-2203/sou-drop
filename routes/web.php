e7d0262d4503bd05c75110158853ffbb55cc2bfd
pedrof-2203 <pedroantoniofpa22@gmail.com>
2025-07-17 22:31:33 -0300

    Routing

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,28 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ChatbotController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// logged-in
+Route::middleware('auth')->group(function(){
+    Route::get('/dashboard', function() {
+        return redirect()->route('products.index');
+    })->name('dashboard');
+
+    // product routes
+    Route::resource('products', ProductController::class);
+
+    // chatbot routes
+    //Route::get('/chatbot', [ChatbotController::class, 'index'])->name('chatbot.index');
+    //Route::get('/chatbot/ask', [ChatbotController::class, 'ask'])->name('chatbot.ask');
+});
+/*
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -15,6 +31,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+});*/
 
 require __DIR__.'/auth.php';
