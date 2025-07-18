233981d0c0a258b1d59473a8e2e0888f718a1cf2
pedrof-2203 <pedroantoniofpa22@gmail.com>
2025-07-17 22:03:53 -0300

    Relations & product migration

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'color',
+        'description',
+        'price',
+        'user_id'
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2025_07_18_005206_create_products_table.php
+++ database/migrations/2025_07_18_005206_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('color');
+            $table->text('description');
+            $table->decimal('price', 10, 2);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
